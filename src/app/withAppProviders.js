21d796cad28c96dfc853c382f94a1a3723aa9122
Dhruv Suhagiya <dhruv.suhagiya11@gmail.com>
2023-10-10 20:32:30 +0530

    update main.yml file

--- src/app/withAppProviders.js
+++ src/app/withAppProviders.js
@@ -1,8 +1,3 @@
-// import createGenerateClassName from '@mui/styles/createGenerateClassName';
-// import jssPreset from '@mui/styles/jssPreset';
-// import { create } from 'jss';
-// import jssExtend from 'jss-plugin-extend';
-// import rtl from 'jss-rtl';
 import Provider from 'react-redux/es/components/Provider';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
