520ba7dd4f8ea3d1332fc035b8874ec8181cb2ae
Jenis Kyada <114799588+JenisKyada@users.noreply.github.com>
2023-12-01 10:27:46 +0530

    Navbar added but responsive need to be add (#5)

--- src/app/main/landing/landing.tsx
+++ src/app/main/landing/landing.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ContentWrapper from "src/app/component/ContentWrapper";
 import Header from "src/app/component/header";
+import Navbar from "src/app/component/Navbar";
 const landing = () => {
   return (
     <ContentWrapper title="Overview">
       <div>
-        <Header data="Landing" />
+        {/* <Header data="Landing" /> */}
+        <Navbar/>
       </div>
     </ContentWrapper>
   );
