ee6adebd6f39d65d4b301a3e07fca367c4b943db
Jenis Kyada <kyadajenis0254@gmail.com>
2023-12-12 16:51:13 +0530

    API Email

--- src/app/store/userManagement.ts
+++ src/app/store/userManagement.ts
@@ -1,4 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+import jsonData from 'src/url.json';
+import { showMessage } from './fuse/messageSlice';
 
 const initialState = {
     data: [{
@@ -26,4 +29,50 @@ const userManagementSlice = createSlice({
 
 export const slice = userManagementSlice.actions;
 export const selectUserManagement = ({ userManagement }) => userManagement;
+
+const URL_BASE_LINK = jsonData.API_LOCAL_URL;
+
+// Send OTP API
+export const sendOTPMailHandler = (data) => async (dispatch) => {
+
+    const payload = {
+        email: data
+    };
+
+    await axios.post(`${URL_BASE_LINK}/otp/sendotp`, payload)
+        .then(res => dispatch(showMessage({ message: res.data.message, variant: "success" })))
+        .catch(err => dispatch(showMessage({ message: err.message, variant: "error" })));
+}
+
+export const verifyOTPMailHandler = (data, navigate) => async (dispatch) => {
+
+    const payload = {
+        email: data.email,
+        otp: data.otp
+    };
+
+    await axios.post(`${URL_BASE_LINK}/otp/validateotp`, payload)
+        .then(res => {
+            dispatch(showMessage({ message: res.data.message, variant: "success" })),
+                navigate("/reset")
+        })
+        .catch(err => dispatch(showMessage({ message: err.response.data.message, variant: "error" })));
+}
+
+export const resetPasswordHandler = (data) => async (dispatch) => {
+
+    const payload = {
+        email: data.email,
+        password: data.password
+    };
+
+    await axios.post(`${URL_BASE_LINK}/user/updatepassword`, payload)
+        .then(res => {
+            dispatch(showMessage({ message: res.data.message, variant: "success" }))
+        })
+        .catch(err => {
+            dispatch(showMessage({ message: err.response.data.message, variant: "error" }))
+        });
+}
+
 export default userManagementSlice.reducer;
