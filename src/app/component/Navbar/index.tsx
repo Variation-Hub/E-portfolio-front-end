637dc31133ddb0d69e8f8796bb211df959a30845
Jenis Kyada <kyadajenis0254@gmail.com>
2023-12-13 17:41:08 +0530

    Footer added

--- src/app/component/Navbar/index.tsx
+++ src/app/component/Navbar/index.tsx
@@ -60,99 +60,43 @@ const Index = () => {
           </div>
         </div>
       </div>
-      <div className={Style.screenshort}></div>
-      <div className={Style.features_container} id="features">
-        <div className={Style.features_list}>
-          <div className={Style.features}>
-            <h2>Features</h2>
-          </div>
-          <ol>
-            <li>Bespoke Management Reporting</li>
-            <li>Online Enrolments- Free template provided</li>
-            <li>Form creations- ILP, Reviews, Learning Plans</li>
-            <li>Video Teaching and Learning Resources for all courses</li>
-            <li>Robust IQA functionality</li>
-            <li>API Integration</li>
-            <li>Easy-to-use platform can be accessed on multiple devices</li>
-            <li>Progress tracking with visual gap analysis</li>
-            <li>Intuitive Layout that is user-friendly</li>
-            <li>Learner Forums</li>
-          </ol>
-        </div>
-        <div className={Style.features_img_div}>
-          <img
-            src="assets/images/svgImage/features.svg"
-            alt="Features"
-            className={Style.features_img}
-          />
-        </div>
-      </div>
-      <div className={Style.whylocker_container}>
-        <div className={Style.why_img_div}>
-          <img
-            src="assets/images/svgImage/why.svg"
-            alt="Why Locker"
-            className={Style.why_img}
-          />
-        </div>
-        <div className={Style.whylocker_list} id="why-locker">
-          <div className={Style.whylocker}>
-            <h2>Why Locker?</h2>
-          </div>
-          <ol>
-            <li>Ofsted and ESFA compliance</li>
-            <li>Real time skills assessment</li>
-            <li>Engaging all users</li>
-            <li>Created by industry experts</li>
-          </ol>
-        </div>
-        <Drawer
-          anchor="right"
-          open={open}
-          onClose={handleClose}
-          sx={{
-            "& .MuiDrawer-paper": {
-              backgroundColor: "#5B718F",
-              width: "60vw",
-            },
-          }}
-        >
-          <ul className="text-white">
-            <IconButton onClick={handleClose} sx={{ textAlign: "left" }}>
-              <CloseIcon />
-            </IconButton>
-            <Divider light />
+      <Drawer
+        anchor="right"
+        open={open}
+        onClose={handleClose}
+        sx={{
+          "& .MuiDrawer-paper": {
+            backgroundColor: "#5B718F",
+            width: "60vw",
+          },
+        }}
+      >
+        <ul className="text-white">
+          <IconButton onClick={handleClose} sx={{ textAlign: "left" }}>
+            <CloseIcon />
+          </IconButton>
+          <Divider light />
+          <li className={`${Style.navbar_link_mobile}`} onClick={handleClose}>
+            About Us
+          </li>
+          <li className={`${Style.navbar_link_mobile}`} onClick={handleClose}>
+            Products
+          </li>
+          <a href="#features">
             <li className={`${Style.navbar_link_mobile}`} onClick={handleClose}>
-              About Us
+              Features
             </li>
+          </a>
+          <a href="#why-locker">
             <li className={`${Style.navbar_link_mobile}`} onClick={handleClose}>
-              Products
+              Why Locker?
             </li>
-            <a href="#features">
-              <li
-                className={`${Style.navbar_link_mobile}`}
-                onClick={handleClose}
-              >
-                Features
-              </li>
-            </a>
-            <a href="#why-locker">
-              <li
-                className={`${Style.navbar_link_mobile}`}
-                onClick={handleClose}
-              >
-                Why Locker?
-              </li>
-            </a>
-            <li
-              onClick={navigateLogin}
-              className={`${Style.navbar_link_mobile}`}
-            >
-              {instance.getAccessToken() ? "Dashboard" : "Log in"}
-            </li>
-          </ul>
-        </Drawer>
-      </div>
+          </a>
+          <li onClick={navigateLogin} className={`${Style.navbar_link_mobile}`}>
+            {instance.getAccessToken() ? "Dashboard" : "Log in"}
+          </li>
+        </ul>
+      </Drawer>
     </>
   );
 };
